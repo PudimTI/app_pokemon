fee8f31924ed13bffde65fa7f1fcae0d4853b815
João Pedro Vaz <pp.joao.mg@hotmail.com>
2024-12-02 01:23:36 -0300

    Update de views

--- src/main/java/com/pokemon/app_pokemon/model/Pokemon.java
+++ src/main/java/com/pokemon/app_pokemon/model/Pokemon.java
@@ -1,11 +1,23 @@
 package com.pokemon.app_pokemon.model;
 
+import java.util.List;
+
 public class Pokemon {
+    private int id;
     private String name;
     private int height;
     private int weight;
 
+
     // Getters e setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
@@ -29,4 +41,5 @@ public class Pokemon {
     public void setWeight(int weight) {
         this.weight = weight;
     }
+
 }
