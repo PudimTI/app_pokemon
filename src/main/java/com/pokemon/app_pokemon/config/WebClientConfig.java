12507f514e9a65dddf07304f25aca4b09af79cf5
João Pedro Vaz <pp.joao.mg@hotmail.com>
2024-12-03 17:27:21 -0300

    Finalização de projeto

--- src/main/java/com/pokemon/app_pokemon/config/WebClientConfig.java
+++ src/main/java/com/pokemon/app_pokemon/config/WebClientConfig.java
@@ -4,10 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Configuration
+@Configuration //classe contém métodos de configuração que vão ser executados pelo Spring
 public class WebClientConfig {
 
-    @Bean
+    @Bean //Usado para definir métodos que produzem objetos gerenciados pelo Spring
     public WebClient webClient(WebClient.Builder builder) {
         return builder.baseUrl("https://pokeapi.co/api/v2").build();
     }
